4d6d4c54ff434fe0066c77ea51e0b8b98b10cebc
Carolina22002703 <carolina.s.b.lopes@hotmail.com>
2021-05-10 16:20:14 +0100

    exercicio_1

--- exercicio_1/analisa_ficheiro/acessorio.py
+++ exercicio_1/analisa_ficheiro/acessorio.py
@@ -1,9 +1,30 @@
+import json
+
 def pede_nome(nome):
-    if (nome[:-3] == "txt"):
+    if (nome[-3]=='t' and nome[-2]=='x' and nome[-1]=='t'):
         print (f'{nome}')
     else:
         print(f'Não existe ficheiro')
+
+def gera_nome(nome):
+    #Abertura do ficheiro e colocação numa lista
+    f = open(nome)
+    ficheiro = f.readlines()
+    f.close()
+
+    dic_numeros = {}
+    for linha in ficheiro:
+        palavra, numero = linha.split()
+        dic_numeros[palavra] = eval(numero)
+
+    with open('teste.json', 'w') as json_file:
+        json.dump(dic_numeros, json_file, indent = 4)
+
     
+#Chamar funções    
 pede_nome("teste.txt")
+gera_nome("teste.txt")
+
+
 
 
--- exercicio_1/analisa_ficheiro/calculos.py
+++ exercicio_1/analisa_ficheiro/calculos.py
@@ -1,13 +1,64 @@
+from collections import Counter
+
 def calcula_linhas(nome):
     #Abertura do ficheiro e colocação numa lista
     f = open(nome)
-    nome = f.read()
+    ficheiro = f.readlines()
     f.close()
 
     #Contar quantas linhas
     count = 0
-    for i in nome.split('\n'):
+    for linha in ficheiro:
         count+=1
-    print (f'{count}')
+    print (f'Número de linhas: {count}')
 
+def calcula_caracteres(nome):
+    #Abertura do ficheiro e colocação numa lista
+    f = open(nome)
+    ficheiro = f.read()
+    f.close()
+
+    #Contar quantos caracteres
+    count1 = 0
+    for i in ficheiro:
+        count1+=1 
+    print(f'Número de caracteres: {count1}')
+
+def calcula_palavra_comprida(nome):
+    #Abertura do ficheiro e colocação numa lista
+    f = open(nome)
+    ficheiro = f.readlines()
+    f.close()
+
+    #Calcular a maior palavra
+    dic_numeros = {}
+    for linha in ficheiro:
+        palavra, numero = linha.split()
+        dic_numeros[palavra] = eval(numero)
+
+
+    palavraMaior = ''
+    for i in dic_numeros:
+        primeira_posicao_palavra = i
+        if(len(primeira_posicao_palavra)>len(palavraMaior)):
+            palavraMaior=primeira_posicao_palavra
+    print(f'Palavra maior: {palavraMaior}')
+
+def calcula_ocorrencia_de_letras(nome):
+    #Abertura do ficheiro e colocação numa lista
+    f = open(nome)
+    ficheiro = f.read()
+    f.close()
+
+    #Calcular ocorrencias de letras
+    counter = Counter(ficheiro)
+    print(f'Dicionário de ocorrências: {counter}')
+   
+        
+        
+#Chamar funções
 calcula_linhas("teste.txt")
+calcula_caracteres("teste.txt")
+calcula_palavra_comprida("teste.txt")
+calcula_ocorrencia_de_letras("teste.txt")
+
