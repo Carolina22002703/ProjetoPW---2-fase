c42b243cfaa7d8c90c53ef110cadb09fd54688d4
marisa-21903611 <marisapintosantoss@gmail.com>
2021-06-05 11:06:21 +0100

    submissao

--- ProjetoPW/admin.py
+++ ProjetoPW/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Usuario
-#from .models import Quizz, Pergunta, Resposta
+from .models import Usuario, Quiz, Pergunta, Resposta, Resultados
 
 
 # Register your models here.
 admin.site.register(Usuario)
-#admin.site.register(Quizz)
+admin.site.register(Quiz)
+admin.site.register(Resultados)
+
+class RespostaInline(admin.TabularInline):
+    model = Resposta
+
+class PerguntaAdmin(admin.ModelAdmin):
+    inlines = [RespostaInline]
+
+admin.site.register(Pergunta, PerguntaAdmin)
+admin.site.register(Resposta)
--- ProjetoPW/models.py
+++ ProjetoPW/models.py
@@ -11,7 +11,6 @@ class Usuario(models.Model):
     def __str__(self):
         return f"{self.nome} {self.apelido} {self.telefone} {self.email} {self.dataNascimento}"
 
-"""
 class Quiz (models.Model):
     OPCOES = [
         ('fácil','fácil'),
@@ -23,83 +22,37 @@ class Quiz (models.Model):
     numeroQuestoes = models.IntegerField()
     tempo = models.IntegerField(help_text="duração do quizz em minutos")
     pontuacaoParaPassar = models.IntegerField(help_text="pontos requeritos em %")
-    dificuldade = models.CharField(max_length=, choices=OPCOES)
+    dificuldade = models.CharField(max_length=50, choices=OPCOES)
 
     def __str__(self):
         return not self.nome + ' ' + self.topico
 
     def get_perguntas(self):
-        pass
-"""
-
-"""
-class Quizz(models.Model):
-    nome = models.CharField(max_length=50)
-    slug = models.SlugField()
-
-    def __str__(self):
-        return self.nome
+        return self.pergunta_set.all()
 
 class Pergunta(models.Model):
-    textoPergunta = models.CharField(max_length=256)
-    publicado = models.BooleanField(default=False)
-    quizz = models.ForeignKey(Quizz, on_delete=models.CASCADE, related_name="perguntas")
+    textoPergunta = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    criado = models.DateTimeField(auto_now_add="True")
 
     def __str__(self):
-        return "{content} - {published}".format(content=self.textoPergunta, published=self.publicado)
+        return str(self.textoPergunta)
+    def get_respostas(self):
+        return self.resposta_set.all()
 
 class Resposta(models.Model):
-    textoResposta = models.CharField(max_length=128)
-    correto = models.BooleanField(default=False)
-    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE, related_name="respostas")
-
-    def __str__(self):
-        return self.textoResposta
-"""
-
-"""class Resposta(models.Model):
-    textoResposta = models.CharField(max_length=100)
-    respostaCorreta = models.BooleanField(default=False)
-    utilizador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.textoResposta
-
-class Pergunta(models.Model):
-    textoPergunta = models.CharField(max_length=100)
-    respostas = models.ManyToManyField(Resposta)
-    points = models.IntegerField()
-    utilizador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.textoPergunta
-
-class Quizz(models.Model):
-    utilizador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=50)
-    data = models.DateTimeField(auto_now_add=True)
-    due = models.DateField()
-    perguntas = models.ManyToManyField(Pergunta)
-
-    def __str__(self):
-        return self.titulo"""
-
-
-"""class Tentativa(models.Model):
-    utilizador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    quiz = models.ForeignKey(Quizz, on_delete=models.CASCADE)
-    pontos = models.PositiveIntegerField()
-    completo = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.utilizador
-
-class Tentar(models.Model):
-    quiz = models.ForeignKey(Quizz, on_delete=models.CASCADE)
-    tentativa = models.ForeignKey(Tentativa, on_delete=models.CASCADE)
+    textoResposta = models.CharField(max_length=50)
+    correta = models.BooleanField(default=False)
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
-    resposta = models.ForeignKey(Resposta, on_delete=models.CASCADE)
+    criado = models.DateTimeField(auto_now_add="True")
 
     def __str__(self):
-        return self.tentativa.utilizador + '-' + self.resposta.textoResposta"""
+        return f"Pergunta: {self.textoPergunta}, Resposta: {self.textoResposta}, Resposta correta {self.correta}"
 
+class Resultados(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    utilizador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pontuacao = models.FloatField()
+
+    def __str__(self):
+        return str(self.pk)
